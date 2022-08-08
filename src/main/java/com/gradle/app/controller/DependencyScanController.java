e29a733fcb43d0b88f500b576164eb246aad0b20
devarajan.r <devarajan.r@mookambikainfo.com>
2022-08-08 11:53:50 +0530

    first commit

---
+++ src/main/java/com/gradle/app/controller/DependencyScanController.java
@@ -0,0 +1,16 @@
+package com.gradle.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DependencyScanController {
+
+    @GetMapping("/report")
+    public String getReport() {
+        return "Report retrieved successfully";
+    }
+
+}
